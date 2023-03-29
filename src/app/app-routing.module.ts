19241b0f43946e6530f6f7e843f7c057b8946b7d
karly22 <carlita.lmj15@gmail.com>
2023-03-28 23:53:22 -0400

    completado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'portafolio', loadChildren: () => import('./portafolio/portafolio.module').then(m => m.PortafolioModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/portafolio/portafolio.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PortafolioRoutingModule } from './portafolio-routing.module';
+import { PortafolioComponent } from './portafolio.component';
+
+
+@NgModule({
+  declarations: [
+    PortafolioComponent
+  ],
+  imports: [
+    CommonModule,
+    PortafolioRoutingModule
+  ]
+})
+export class PortafolioModule { }
